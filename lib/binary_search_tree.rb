97421210ba76cfb7417d0a6a93bce0dc15791272
Chris Bolas <tanlaan33@gmail.com>
2021-07-12 18:59:36 -0500

    Add Node

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,10 @@
+class Node
+    attr_accessor(:data, :left, :right)
+
+    def initialize(data, left, right)
+        @data = data
+        @left = left
+        @right = right
+    end
+        
+end
