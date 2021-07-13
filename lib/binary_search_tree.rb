dc0d6329240c0f23a32ae77021cc4f791b99cdaa
Chris Bolas <tanlaan33@gmail.com>
2021-07-12 19:05:34 -0500

    Fix default values on Node

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -6,14 +6,21 @@ class Tree
     end
     
     def build_tree(values)
-        # Return a balanced binary tree
+        # Recursivly return a balanced binary tree
+        
+        # Sort our values....
+        values.sort!
+
+        # Create a node for our middle value
+        root = Node.new()
+
     end
 end
 
 class Node
     attr_accessor(:data, :left, :right)
 
-    def initialize(data, left, right)
+    def initialize(data = nil, left = nil, right = nil)
         @data = data
         @left = left
         @right = right
