317df6879f34f64c528a40c75b811a81f0e41a79
Chris Bolas <tanlaan33@gmail.com>
2021-07-12 19:34:32 -0500

    Add build_tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,5 +1,5 @@
 class Tree
-    @attr_reader(:root)
+    attr_reader(:root)
 
     def initialize(values)
         @root = self.build_tree(values)
@@ -7,13 +7,22 @@ class Tree
     
     def build_tree(values)
         # Recursivly return a balanced binary tree
+        p values
+
+        # Base cases
+        return nil if values.nil? || values.length == 0
+        return Node.new(values[0]) if values.length == 1
         
-        # Sort our values....
+        # Sort our values...
         values.sort!
 
         # Create a node for our middle value
-        root = Node.new()
-
+        halfway = values.length / 2
+        return Node.new(
+            values[halfway], 
+            build_tree(values.slice(0, halfway)),
+            build_tree(values.slice(halfway + 1, values.length))
+        )
     end
 end
 
