ac1bedb33c478ad4ec77428be687c99fec289cf0
Chris Bolas <tanlaan33@gmail.com>
2021-07-12 19:01:44 -0500

    Add Tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,3 +1,15 @@
+class Tree
+    @attr_reader(:root)
+
+    def initialize(values)
+        @root = self.build_tree(values)
+    end
+    
+    def build_tree(values)
+        # Return a balanced binary tree
+    end
+end
+
 class Node
     attr_accessor(:data, :left, :right)
 
