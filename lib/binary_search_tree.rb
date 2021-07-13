f4c868e987e539af9517cd8f365b48f40333906c
Chris Bolas <tanlaan33@gmail.com>
2021-07-12 20:00:18 -0500

    Add insert

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -24,6 +24,24 @@ class Tree
             build_tree(values.slice(halfway + 1, values.length))
         )
     end
+
+    def insert(value)
+        # TODO: Remove assumption of no duplicate values
+
+        current_node = @root
+        loop do
+            if current_node.data > value && current_node.left.nil?
+                current_node.left = Node.new(value)
+                break
+            elsif current_node.data < value && current_node.right.nil?
+                curren_node.right = Node.new(value)
+                break
+            end
+            current_node = current_node.left if current_node.data > value
+            current_node = current_node.right if current_node.data < value
+        end
+        
+    end
 end
 
 class Node
