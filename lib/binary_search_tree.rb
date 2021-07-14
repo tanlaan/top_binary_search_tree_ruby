c47a4e5a4624bcaaf0cb55adab676fbb35e60b84
Chris Bolas <tanlaan33@gmail.com>
2021-07-14 05:53:35 -0700

    Add pretty_print

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -155,6 +155,12 @@ class Tree
     def rebalance
         @root = build_tree(self.level_order)
     end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
 end
 
 class Node
